3d759021870ed2e3d9f89bc03f24b4db8204fc1f
radoslav hristov <bmwto@outlook.com>
2022-11-29 12:46:40 +0200

    Add view page of AllTown

---
+++ MyWebProject.Core/Services/IServices/IHomeService.cs
@@ -0,0 +1,14 @@
+﻿using MyWebProject.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebProject.Core.Services.IServices
+{
+    public interface IHomeService
+    {
+        public Task<IEnumerable<Pictures>> AllPicture();
+    }
+}
---
+++ MyWebProject.Core/Services/Services/HomeServices.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using My_Web_Project_LandMarks_.Infrastructure.Data.Common;
+using MyWebProject.Core.Services.IServices;
+using MyWebProject.Infrastructure.Data.Models;
+
+namespace MyWebProject.Core.Services.Services
+{
+    public class HomeServices : IHomeService
+    {
+        public readonly IRepository repo;
+
+        public HomeServices(IRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public async Task<IEnumerable<Pictures>> AllPicture()
+        {
+            var result = await repo.All<Pictures>()
+               .Where(x => x.TownId == null && x.LandMarkId == null && x.JourneyId == null)
+               .ToListAsync();
+        
+           return result;
+        }
+    }
+}
