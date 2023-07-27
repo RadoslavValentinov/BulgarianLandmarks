7b37bf74190b5901de7916ad6015f4765136e033
radoslav hristov <bmwto@outlook.com>
2023-07-27 13:57:39 +0300

    ##New property of model and  added test project of buisnes logic##

--- MyWebProject.Infrastructure/Data/Models/Cultural events.cs
+++ MyWebProject.Infrastructure/Data/Models/Cultural events.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace My_Web_Project_LandMarks_.Infrastructure.Data.Models
+namespace MyWebProject.Infrastructure.Data.Models
 {
     public class Cultural_events
     {
