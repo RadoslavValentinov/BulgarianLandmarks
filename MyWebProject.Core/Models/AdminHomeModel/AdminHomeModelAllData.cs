63a963f9a560b962b6d512654b0cb550a4813acf
radoslav hristov <bmwto@outlook.com>
2024-12-19 21:21:31 +0200

    # Add Admin  model ###

---
+++ MyWebProject.Core/Models/AdminHomeModel/AdminHomeModelAllData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebProject.Core.Models.AdminHomeModel
+{
+    public class AdminHomeModelAllData
+    {
+        public int CountOfUsers { get; set; }
+        public int NotActivUsers { get; set; }
+
+        public int CountOfEvents { get; set; }
+        public int NotActiveEvents { get; set; }
+
+        public int CountOfFacts { get; set; }
+        public int NotActiveFacts { get; set; }
+
+        public int CountOfCategory { get; set; }
+        public int NotActiveCategory { get; set; }
+
+        public int CountOfJourney { get; set; }
+        public int NotActiveJourney { get; set; }
+
+        public int CountOfTown  { get; set; }
+        public int NotActiveTown { get; set; }
+
+        public int CountOfLandmarks  { get; set; }
+        public int NotActivLandMark { get; set; }
+
+        public int CountOfPictures  { get; set; }
+        public int NotActivPictures { get;set; }
+    }
+}
